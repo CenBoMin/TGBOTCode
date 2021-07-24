7b969f95eeb129a9b98b21a7472ff6036090ab14
Cenbomin <2564885669@qq.com>
2021-07-24 10:20:48 +0800

    每小时处理-txstock脚本互助码

--- txstock.js
+++ txstock.js
@@ -1,1 +1,1 @@
-[{"uid":"1285467394","code":["6gGAazjOf1Hm5Yrn00","6gGAazjOf1Hm5Yrn00&Kj8AazjOf11EiWIxGd","EZd119lOf1NIEYxLu4&qGa119lOf1YacoUtPy"]}]
+[{"uid":"664090780","code":["BytdWElOf1rf23635b&jHUSzElOf1re774711"]},{"uid":"1285467394","code":["6gGAazjOf1Hm5Yrn00","6gGAazjOf1Hm5Yrn00&Kj8AazjOf11EiWIxGd","EZd119lOf1NIEYxLu4&qGa119lOf1YacoUtPy"]}]
